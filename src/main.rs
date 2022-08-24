b1036d83f84cc034b57d6874d8c8313d8c4f4e3c
kyunghyunHan <ekfqlwcjswl@gmail.com>
2022-08-24 11:10:55 +0900

    add

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate piston_window;
 extern crate rand;
 mod draw;
+mod game;
 mod snake;
 fn main() {
     println!("Hello, world!");
--- src/snake.rs
+++ src/snake.rs
@@ -88,4 +88,38 @@ impl Snake {
         self.direction
     }
     //17:45
+
+    pub fn next_head(&self, dir: Option<Direction>) -> (i32, i32) {
+        let (head_x, head_y): (i32, i32) = self.head_position();
+
+        let mut moving_dir = self.direction;
+        match dir {
+            Some(d) => moving_dir = d,
+            None => {}
+        }
+        match moving_dir {
+            Direction::Up => (head_x, head_y - 1),
+            Direction::Down => (head_x, head_y + 1),
+            Direction::Left => (head_x - 1, head_y),
+            Direction::Right => (head_x + 1, head_y),
+        }
+    }
+    pub fn restore_tail(&mut self) {
+        let blk = self.tail.clone().unwrap();
+        self.body.push_back(blk);
+    }
+
+    pub fn overlap_tail(&self, x: i32, y: i32) -> bool {
+        let mut ch = 0;
+        for block in &self.body {
+            if x == block.x && y == block.y {
+                return true;
+            }
+            ch += 1;
+            if ch == self.body.len() - 1 {
+                break;
+            }
+        }
+        return false;
+    }
 }
