523f9d0c4fe454a44aa8135bd7e376a32b4cd344
kyunghyunHan <ekfqlwcjswl@gmail.com>
2022-08-24 10:57:32 +0900

    add

--- src/snake.rs
+++ src/snake.rs
@@ -5,7 +5,7 @@ use std::collectons::LinkedList;
 use draw::draw_block;
 
 const SNAKE_COLOR: Color = [0.00, 0.80, 0.00, 1.0];
-
+#[derive(Copy, Clone, PartialEq)]
 pub enum Direction {
     Up,
     Down,
@@ -22,6 +22,7 @@ impl Direction {
         }
     }
 }
+#[derive(Debug, Clone)]
 struct Block {
     x: i32,
     y: i32,
@@ -44,4 +45,47 @@ impl Snake {
             tail: None,
         }
     }
+    pub fn draw(&self, con: &Context, g: &mut G2d) {
+        for block in &self.body {
+            draw_block(SNAKE_COLOR, block.x, block.y, con, g);
+        }
+    }
+    pub fn head_position(&self) -> (i32, i32) {
+        let head_block = self.body.front().unwrap();
+        (head_block.x, head_block.y)
+    }
+
+    pub fn move_forward(&mut self, dir: Option<Direction>) {
+        match der {
+            Some(d) => self.direction = d,
+            None => (),
+        }
+        let (last_x, last_y): (i32, i32) = self.head_position();
+
+        let new_block = match self.direction {
+            Direction::Up => Blocl {
+                x: last_x,
+                y: last_y - 1,
+            },
+            Direction::Down => Block {
+                x: last_x,
+                y: last_y + 1,
+            },
+            Direction::Left => Block {
+                x: last_x - 1,
+                y: last_y,
+            },
+            Direction::Right => Block {
+                x: last_x + 1,
+                y: last_y,
+            },
+        };
+        self.body.push_front(new_block);
+        let removed_block = self.body.pop_back().unwrap();
+        self.tail = Some(removed_block);
+    }
+    pub fn head_direction(&self) -> Direction {
+        self.direction
+    }
+    //17:45
 }
